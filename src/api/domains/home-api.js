6f7f3bf217e776b2fed6604d3ad4aceccdfaa97f
Solijonov Qodirjon <solijonovqodirjon@gmail.com>
2023-01-19 22:42:05 +0500

    style: files formatted

---
+++ src/api/domains/home-api.js
@@ -0,0 +1,12 @@
+const banner = {
+  id: 1,
+  url: 'https://www.shutterstock.com/image-illustration/electric-car-drive-on-wind-600w-1972202852.jpg',
+};
+
+const banners = [banner, banner, banner, banner];
+
+export const HomeApi = {
+  fetchBanners: async () => {
+    return banners;
+  },
+};
