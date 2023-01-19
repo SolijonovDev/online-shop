db7ca35140644b4c5f9768b823b8e115d9a72a8c
Solijonov Qodirjon <solijonovqodirjon@gmail.com>
2023-01-20 00:02:54 +0500

    feat: Typography com added to Components

---
+++ src/components/Typography/Typography.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+import styles from '@/components/Typography/Typography.module.scss';
+
+export const Typography = props => {
+  const { children } = props;
+  const [] = useState();
+  return <p>{Children}</p>;
+};
