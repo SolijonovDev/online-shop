786b4db337f6b1f02bcf8938a241db6d6c8de995
Solijonov Qodirjon <solijonovqodirjon@gmail.com>
2023-01-19 22:38:35 +0500

    feat: react-query package added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,28 @@
 import { appWithTranslation } from 'next-i18next';
-import { Fragment } from 'react';
+import { Fragment, useRef } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { Hydrate } from 'react-query/hydration';
 
 import { MainLayout } from '@/layout/MainLayout/MainLayout';
 import '@/styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
+  const queryClient = useRef();
+  if (!queryClient.current) {
+    queryClient.current = new QueryClient();
+  }
+
   return (
     <Fragment>
-      <MainLayout>
-        <Component {...pageProps} />
-      </MainLayout>
+      <QueryClientProvider client={queryClient.current}>
+        <Hydrate state={pageProps.dehydrated}>
+          <MainLayout>
+            <Component {...pageProps} />
+          </MainLayout>
+          {process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
+        </Hydrate>
+      </QueryClientProvider>
     </Fragment>
   );
 };
