8beb8ff3305381f986418830491a5b2b96524b6a
Solijonov Qodirjon <solijonovqodirjon@gmail.com>
2023-01-21 00:22:27 +0500

    refactor: minor change

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,19 @@
+import cn from 'classnames';
+
+import styles from '@/components/Button/Button.module.scss';
+
 export const Button = props => {
-  const { children } = props;
-  return <button>{children}</button>;
+  const { children, variant = 'contained', color = 'primary', ...rest } = props;
+  return (
+    <button
+      className={cn({
+        [styles.button]: true,
+        [styles[variant]]: variant,
+        [styles[color]]: color,
+      })}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
 };
