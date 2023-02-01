2157ddb8826958120d301c3ec39dc4ee65f5148e
Solijonov Qodirjon <solijonovqodirjon@gmail.com>
2023-02-01 23:49:28 +0500

    feat: add svgr/webpack package

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
